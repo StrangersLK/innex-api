0388cb17f3565ffd035bce9d2c9fe16abeb04bb1
Milinda Laknath Arambawela <milindalaknath@gmail.com>
2020-11-29 21:26:55 +0530

    Init commit

---
+++ src/main/java/com/kandy/innex/InnexApplication.java
@@ -0,0 +1,13 @@
+package com.kandy.innex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kandy/innex/InnexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kandy.innex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
